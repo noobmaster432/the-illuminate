42a1e52607b5c488fa2220799abdcfc24997073c
Gyanendra Tiwari <mrgyan432@gmail.com>
2023-01-04 12:01:04 +0530

    initial commit

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -14,8 +14,7 @@ async function SearchPage({ searchParams }: Props) {
 
   return (
     <div>
-        <h1 className="headerTitle">Search Results for: {searchParams?.term}</h1>
-        
+        <h1 className="headerTitle">Search Results for: {searchParams?.term}</h1>      
         <NewsList news={news} />
     </div>);
 }
